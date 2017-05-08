3772778b5f60d65a28cd19a9ef33bda6e0e41dc4
David Álvarez <davidalvarezcalvo@gmail.com>
2017-05-08 15:10:10 +0200

    2

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -5,6 +5,138 @@ let node = {
    name: "foo"
 };
 
-let { type,name  = node;
+let { type,name } = node;
 
   console.log(type); //"Identifier"
+// dont forget initializer !!
+
+// destructuring assignment
+
+// nested object destructuring
+let node2 = {
+        type: "Identifier",
+        name: "foo",
+        loc: {
+            start: {
+                line: 1,
+                column: 1
+            },
+            end: {
+                line: 1,
+                column: 4
+            }
+        }
+    };
+
+let { loc: { end }} = node2;
+console.log(end.column); //4
+
+// Array destructuring
+
+let colors = [ "red", "green" , "blue" ];
+
+let [ firstColour, secondColour ] = colors;
+
+console.log(firstColor);
+
+// Array destructuring with default value
+let colors = [ "red" ];
+
+let [ firstColor, secondColor = "green" ] = colors;
+
+console.log(firstColor);        // "red"
+console.log(secondColor);       // "green"
+
+// nested destructuring
+let colors = [ "red", [ "green", "lightgreen" ], "blue" ];
+
+// later
+
+let [ firstColor, [ secondColor ] ] = colors;
+
+console.log(firstColor);        // "red"
+console.log(secondColor);       // "green"
+
+// Rest Items
+let colors = [ "red", "green", "blue" ];
+
+let [ firstColor, ...restColors ] = colors;
+
+console.log(firstColor);        // "red"
+console.log(restColors.length); // 2
+console.log(restColors[0]);     // "green"
+console.log(restColors[1]);     // "blue"
+
+// cloning an array in ECMAScript 6
+let colors = [ "red", "green", "blue" ];
+let [ ...clonedColors ] = colors;
+
+// mixed destructuring
+let node = {
+        type: "Identifier",
+        name: "foo",
+        loc: {
+            start: {
+                line: 1,
+                column: 1
+            },
+            end: {
+                line: 1,
+                column: 4
+            }
+        },
+        range: [0, 3]
+    };
+
+let {
+    loc: { start },
+    range: [ startIndex ]
+} = node;
+
+console.log(start.line);        // 1
+console.log(start.column);      // 1
+console.log(startIndex);        // 0
+
+// destructured parameters
+// properties on options represent additional parameters
+function setCookie(name, value, options) {
+
+    options = options || {};
+
+    let secure = options.secure,
+        path = options.path,
+        domain = options.domain,
+        expires = options.expires;
+
+    // code to set the cookie
+}
+
+// third argument maps to options
+setCookie("type", "js", {
+    secure: true,
+    expires: 60000
+});
+
+// Alternative destructured parameters
+function setCookie(name, value, { secure, path, domain, expires }) {
+
+    // code to set the cookie
+}
+
+setCookie("type", "js", {
+    secure: true,
+    expires: 60000
+});
+
+// Default value for Destructured parameters
+function setCookie(name, value,
+    {
+        secure = false,
+        path = "/",
+        domain = "example.com",
+        expires = new Date(Date.now() + 360000000)
+    } = {}
+) {
+
+    // ...
+}
