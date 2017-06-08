e00dd4720572dd6cf2896eb09783d1cd5b4ff18a
David Álvarez <davidalvarezcalvo@gmail.com>
2017-06-08 12:41:49 +0200

    segunda

---
+++ angular2/src/app/components/body.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-body',
+  templateUrl: 'body.component.html'
+})
+export class BodyComponent  {  }
